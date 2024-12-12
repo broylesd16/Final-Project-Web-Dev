5751dcaf41702d7e8498e9730aa7a2cdf048c344
David Broyles <broylesd16@mycu.concord.edu>
2024-12-11 19:47:01 -0500

    WIP

--- register.php
+++ register.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Register - Betty's Book Banning</title>
+    <title>Register - GoatKeeper</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
